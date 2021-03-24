7d6a2610227a706a440fb928c976614de6666fe8
lipengzhou <lpzmail@163.com>
2021-03-24 18:33:19 +0800

    test: feature-编辑任务

--- tests/feature/TodoApp.spec.js
+++ tests/feature/TodoApp.spec.js
@@ -187,3 +187,99 @@ describe('切换所有任务的完成状态', () => {
     expect(toggleAll.element.checked).toBeFalsy()
   })
 })
+
+describe('编辑任务', () => {
+  test('双击任务项文本，应该获得编辑状态', async () => {
+    await wrapper.setData({
+      todos: [
+        { id: 1, text: 'play', done: true }
+      ]
+    })
+    const todoText = wrapper.find('[data-testid="todo-text"]')
+    const todoItem = wrapper.find('[data-testid="todo-item"]')
+
+    // 双击之前确认任务项不是编辑状态
+    expect(todoItem.classes('editing')).toBeFalsy()
+
+    // 双击任务项文本
+    await todoText.trigger('dblclick')
+
+    // 双击之后，任务项应该获得编辑状态
+    expect(todoItem.classes('editing')).toBeTruthy()
+  })
+
+  test('修改任务项文本敲回车之后，应该保存修改以及取消编辑状态', async () => {
+    await wrapper.setData({
+      todos: [
+        { id: 1, text: 'play', done: true }
+      ]
+    })
+    const todoText = wrapper.find('[data-testid="todo-text"]')
+    const todoItem = wrapper.find('[data-testid="todo-item"]')
+    const todoEdit = wrapper.find('[data-testid="todo-edit"]')
+
+    // 双击任务项文本获得编辑状态
+    await todoText.trigger('dblclick')
+
+    // 修改任务项文本
+    const text = 'hello'
+    await todoEdit.setValue(text)
+
+    // 回车保存
+    await todoEdit.trigger('keyup.enter')
+
+    // 任务项文本被修改了
+    expect(todoText.text()).toBe(text)
+    // 任务项的编辑状态取消了
+    expect(todoItem.classes('editing')).toBeFalsy()
+  })
+
+  test('清空任务项文本，保存编辑应该删除任务项', async () => {
+    await wrapper.setData({
+      todos: [
+        { id: 1, text: 'play', done: true }
+      ]
+    })
+    const todoText = wrapper.find('[data-testid="todo-text"]')
+    const todoItem = wrapper.find('[data-testid="todo-item"]')
+    const todoEdit = wrapper.find('[data-testid="todo-edit"]')
+
+    // 双击任务项文本获得编辑状态
+    await todoText.trigger('dblclick')
+
+    // 修改任务项文本
+    await todoEdit.setValue('')
+
+    // 回车保存
+    await todoEdit.trigger('keyup.enter')
+
+    // 内容被清空了，任务项应该被删除
+    expect(todoItem.exists()).toBeFalsy()
+  })
+
+  test('修改任务项文本按下 ESC 后，应该取消编辑状态以及任务项文本保持不变', async () => {
+    await wrapper.setData({
+      todos: [
+        { id: 1, text: 'play', done: true }
+      ]
+    })
+    const todoText = wrapper.find('[data-testid="todo-text"]')
+    const todoItem = wrapper.find('[data-testid="todo-item"]')
+    const todoEdit = wrapper.find('[data-testid="todo-edit"]')
+
+    // 双击任务项文本获得编辑状态
+    await todoText.trigger('dblclick')
+
+    // 修改任务项文本
+    const originText = todoText.text()
+    await todoEdit.setValue('hello')
+
+    // ESC 取消
+    await todoEdit.trigger('keyup.esc')
+
+    // 取消保存，文本不变，任务项还在，并且取消编辑状态
+    expect(todoItem.exists()).toBeTruthy()
+    expect(todoText.text()).toBe(originText)
+    expect(todoItem.classes('editing')).toBeFalsy()
+  })
+})
